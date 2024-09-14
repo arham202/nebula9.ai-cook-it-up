4839e8a065471c51fd9778317f60bbc5aee80191
Arham Alam <arham.alam.21cse@bmu.edu.in>
2024-09-14 19:38:02 +0530

    structure change

--- server/app.js
+++ server/app.js
@@ -10,9 +10,13 @@ const app = express();
 
 app.use(cors("*"));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Cook It Up API." });
