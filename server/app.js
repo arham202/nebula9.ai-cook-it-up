b7607cc829f155af406843e198c57219617ef952
Arham Alam <arham.alam.21cse@bmu.edu.in>
2024-09-14 19:45:34 +0530

    package updated

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 app.get("/", (req, res) => {
